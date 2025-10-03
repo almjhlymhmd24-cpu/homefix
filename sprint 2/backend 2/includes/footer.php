5aead6366507a15b281f878a9d28b3caae3fcf7b
Lamis <almjhlymhmd24@gmail.com>
2025-10-03 08:19:14 +0300

    Keep only sprint 1

--- sprint 2/backend 2/includes/footer.php
+++
@@ -1,8 +0,0 @@
-    </main>
-    <footer class="footer">
-        <div class="container">
-            <p>&copy; 2024 نظام إدارة المستخدمين. جميع الحقوق محفوظة.</p>
-        </div>
-    </footer>
-</body>
-</html>
